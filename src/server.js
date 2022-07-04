506d2ba7ccd5f9bfa681fd066e15485222c1624a
John Bridges <jbridges373@gmail.com>
2022-07-04 15:20:21 +0100

    writing codes

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require("db/connection"); // instantly run database connection
+const express = require("express"); // pull in all of express module
+const app = express(); // create webserver constant to manipulate
+const port = process.env.PORT || 5001; // store either supplied port or 5001
+
+app.use(express.json()); // parse all request as if they are JSON, sends all responses as JSON
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+// listening on port 5001 or provided port on current location (localhost)
